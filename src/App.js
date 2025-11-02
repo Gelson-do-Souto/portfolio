b8f79c88184f9a97a4c2081d05d846f7f48547c9
Gelson do Souto <gsonfut@gmail.com>
2025-11-02 16:40:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -483,7 +483,7 @@ const App = () => {
                             >
                                 {/* CORREÇÃO DA IMAGEM APLICADA AQUI */}
                                 <img
-                                    src="/images/ICONE.PNg"
+                                    src="build/images/ICONE.PNg"
                                     alt="Gelson do Souto - Perfil"
                                     className="rounded-none w-64 h-64 object-cover mx-auto border-2 border-green-500 filter grayscale contrast-120 hover:filter-none transition-filter duration-500"
                                 />
